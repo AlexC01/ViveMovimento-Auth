a874d72d2281eefa1133ed52807dc90c24b3fa2e
AlexC01 <alexcuadraxxx@gmail.com>
2020-12-13 21:01:35 -0600

    listo

--- ViveMovimento/authapi/urls.py
+++ ViveMovimento/authapi/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
-    path('rest-auth/google/', GoogleLogin.as_view(), name='fb_login'),
+    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
 ]
