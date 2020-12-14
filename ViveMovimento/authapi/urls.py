f5c5a801df821f6b443998f4c0fb0db48f802c46
AlexC01 <alexcuadraxxx@gmail.com>
2020-12-13 21:00:46 -0600

    agregando google

--- ViveMovimento/authapi/urls.py
+++ ViveMovimento/authapi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
-from .views import FacebookLogin
+from .views import *
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login')
+    path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
+    path('rest-auth/google/', GoogleLogin.as_view(), name='fb_login'),
 ]
--- ViveMovimento/authapi/views.py
+++ ViveMovimento/authapi/views.py
@@ -3,8 +3,15 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
+
+
 
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
 
